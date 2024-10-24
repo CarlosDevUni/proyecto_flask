b9f1148490de64d2e7a2e5000bf8f685aff3287e
Carlos Berger <carlos@mail.com>
2024-10-24 08:27:39 -0300

    [ADD] Ruta /persons - Métodos PUT y DELETE

--- api/__init__.py
+++ api/__init__.py
@@ -59,3 +59,54 @@ def create_person():
     
     else:
         return jsonify( {"message" : "Error: ya existe una persona con ese dni"}), 400
+    
+@app.route('/persons/<int:id>', methods = ['PUT'])
+def update_person(id):
+    # simula UPDATE ... SET ... WHERE
+    # Control de datos recibidos (se debería verificar que existan todas las claves, que sean del tipo correcto, etc)
+    name = request.get_json()["name"]
+    surname = request.get_json()["surname"]
+    email = request.get_json()["email"]
+    dni = request.get_json()["dni"]
+
+    # Ubicar el recurso con el id correspondiente
+    index_person = [index for index in range(len(persons_db)) if persons_db[index]["id"]== id]
+
+    # Controlar si existe
+    if len(index_person) > 0:
+        index = index_person[0] 
+        
+        # Aquí se deberían implementar otros controles
+        # Por ejemplo si existen columnas que restringen valor único
+        # En este caso, no se podría actualizar cualquier valor en el dni
+        # Correspondería controlar que el nuevo valor no pertenece a otro registro
+        # (sin implementar acá)
+        persons_db[index]["dni"] = dni
+        persons_db[index]["name"] = name
+        persons_db[index]["surname"] = surname
+        persons_db[index]["email"] = email
+
+        uptaded = persons_db[index]
+
+        return jsonify({"person" : uptaded}), 200
+    else:
+        return jsonify( {"message" : "Error: no existe una persona con ese id"}), 400
+    
+@app.route('/persons/<int:id>', methods = ['DELETE'])
+def delete_person(id):
+    # simula DELETE ... FROM ... WHERE
+    
+    # Ubicar el recurso con el id correspondiente
+    index_person = [index for index in range(len(persons_db)) if persons_db[index]["id"]== id]
+
+    # Controlar si existe
+    if len(index_person) > 0:
+        index = index_person[0] 
+        
+        # La confirmación para eliminar se implementa en las interfaces de usuario
+        # Una vez enviada la solicitud al servidor, la eliminación se efectúa directamente
+        deleted = persons_db.pop(index)
+
+        return jsonify({"person" : deleted}), 200
+    else:
+        return jsonify( {"message" : "Error: no existe una persona con ese id"}), 400    
